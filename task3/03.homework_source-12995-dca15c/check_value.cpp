46670eb8a5476756d079ddde1a4fa1bce86c65d3
Juri <jur@pospelow.de>
2023-10-21 12:33:40 +0200

    task3: some clean

--- task3/03.homework_source-12995-dca15c/check_value.cpp
+++ task3/03.homework_source-12995-dca15c/check_value.cpp
@@ -2,27 +2,27 @@
 
 int main() {
 
-	const int target_value = 54;
-	int current_value = 0;
-	bool not_win = true;
+    const int target_value = 54;
+    int current_value = 0;
+    bool not_win = true;
 
-	std::cout << "Enter your guess:" << std::endl;
+    std::cout << "Enter your guess:" << std::endl;
 
-	do {
-		std::cin >> current_value;
+    do {
+        std::cin >> current_value;
 
-		if (current_value < target_value) {
-			std::cout << "less than " << current_value << std::endl;
-		}
-		else if (current_value > target_value) {
-			std::cout << "greater than " << current_value << std::endl;
-		}
-		else {
-			std::cout << "you win!" << std::endl;
-			break;
-		}
+        if (current_value < target_value) {
+            std::cout << "less than " << current_value << std::endl;
+        }
+        else if (current_value > target_value) {
+            std::cout << "greater than " << current_value << std::endl;
+        }
+        else {
+            std::cout << "you win!" << std::endl;
+            break;
+        }
 
-	} while(true);
+    } while(true);
 
-	return 0;
+    return 0;
 }
--- task3/03.homework_source-12995-dca15c/random_value.cpp
+++ task3/03.homework_source-12995-dca15c/random_value.cpp
@@ -5,13 +5,13 @@
 
 int main() {
 
-	const int max_value = 100;
+    const int max_value = 100;
 
-	std::srand(std::time(nullptr)); // use current time as seed for random generator
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
 
-	const int random_value = std::rand() % 100;
+    const int random_value = std::rand() % 100;
 
-	std::cout << random_value << std::endl;
+    std::cout << random_value << std::endl;
 
-	return 0;
+    return 0;
 }
