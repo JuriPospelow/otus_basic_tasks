37ef833049dfad67ea8f9fa9a3023bf46253be24
Juri <jur@pospelow.de>
2023-10-21 13:26:53 +0200

    task3: introduce high_scores.hpp

--- task3/03.homework_source-12995-dca15c/check_value.cpp
+++ task3/03.homework_source-12995-dca15c/check_value.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "random_value.hpp"
+#include "high_scores.hpp"
 
 int main() {
 
@@ -7,23 +8,38 @@ int main() {
     unsigned int current_value = 0;
     bool not_win = true;
 
+    ask_name();
+
     std::cout << "Enter your guess:" << std::endl;
 
+    unsigned attempts_cnt{};
     do {
         std::cin >> current_value;
+        if (std::cin.fail()) {
+            std::cout << "Bad value!" << std::endl;
+            return -1;
+        }
 
         if (current_value < target_value) {
             std::cout << "greater than " << current_value << std::endl;
+            ++attempts_cnt;
         }
         else if (current_value > target_value) {
             std::cout << "less than " << current_value << std::endl;
+            ++attempts_cnt;
         }
         else {
-            std::cout << "you win!" << std::endl;
+            std::cout << "you win! attempts = " << attempts_cnt << std::endl;
             break;
         }
 
     } while(true);
+
+    put_attempts_count(attempts_cnt);
+    write2file();
+    read4file();
+
+
 
     return 0;
 }
---
+++ task3/03.homework_source-12995-dca15c/high_scores.hpp
@@ -0,0 +1,11 @@
+#ifndef HIGH_SCORE_HPP
+#define HIGH_SCORE_HPP
+
+void ask_name();
+void put_attempts_count(unsigned attempts_cnt = 0);
+// Write new high score to the records table
+void write2file();
+// Read the high score file and print all results
+void read4file();
+
+#endif
