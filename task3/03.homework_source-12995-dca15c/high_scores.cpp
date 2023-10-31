a748b70c08379429838524be7ec518866fe2ece2
Juri <jur@pospelow.de>
2023-10-31 09:27:17 +0100

    task3: introduce fill_container()

--- task3/03.homework_source-12995-dca15c/high_scores.cpp
+++ task3/03.homework_source-12995-dca15c/high_scores.cpp
@@ -2,15 +2,21 @@
 #include <fstream>
 #include <string>
 
+#include <filesystem>
+
 #include <map>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
+using namespace std::filesystem;
 
 std::string user_name;
 const std::string high_scores_filename = "high_scores.txt";
 unsigned attempts_count{};
+
+typedef multimap<string, int> MultiMap;
+MultiMap multimap_high_score;
 
 void ask_name()
 {
@@ -24,31 +30,46 @@ void put_attempts_count(unsigned attempts_cnt)
     attempts_count = attempts_cnt;
 }
 
-int clean_file()
+int fill_container()
 {
-    typedef multimap<string, int> MultiMap;
-    MultiMap multimap_high_score;
-    std::fstream file{high_scores_filename};
-    if (!file.is_open()) {
+    std::fstream read_file{high_scores_filename};
+    if (!read_file.is_open()) {
        std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
        return -1;
     }
-    // std::cout << "High scores table:" << std::endl;
+
     std::string username;
     int high_score = 0;
     while (true) {
         // Read the username first
-        file >> username;
+        read_file >> username;
         // Read the high score next
-        file >> high_score;
+        read_file >> high_score;
         // Ignore the end of line symbol
-        file.ignore();
+        read_file.ignore();
 
-        if (file.fail()) {
+        if (read_file.fail()) {
             break;
         }
         multimap_high_score.insert(pair<string, int>(username, high_score));
     }
+
+    read_file.close();
+    return 0;
+}
+
+int clean_file()
+{
+    fill_container();
+    remove(high_scores_filename);
+
+    std::ofstream file{high_scores_filename};
+    if (!file.is_open()) {
+        std::cout << "Failed to open file for write: " << high_scores_filename << "!" << std::endl;
+        return -1;
+    }
+
+#if 1
     // printing multimap multimap_high_score
     MultiMap::iterator itr;
     // cout << "\tUser Name\tHIGH SCORE\n";
@@ -76,7 +97,7 @@ int clean_file()
         file << tmp_value << endl;
         tmp_value = numeric_limits<int>::max();
     }
-
+#endif
     return 0;
     // cout << endl;
 }
@@ -106,31 +127,7 @@ int write2file()
 // Read the high score file and print all results
 int read4file()
 {
-    typedef multimap<string, int> MultiMap;
-    MultiMap multimap_high_score;
-    std::ifstream in_file{high_scores_filename};
-    if (!in_file.is_open()) {
-       std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
-       return -1;
-    }
-    std::cout << "High scores table:" << std::endl;
-    std::string username;
-    int high_score = 0;
-    while (true) {
-        // Read the username first
-        in_file >> username;
-        // Read the high score next
-        in_file >> high_score;
-        // Ignore the end of line symbol
-        in_file.ignore();
-
-        if (in_file.fail()) {
-            break;
-        }
-        multimap_high_score.insert(pair<string, int>(username, high_score));
-        // Print the information to the screen
-        // std::cout << username << '\t' << high_score << std::endl;
-    }
+    fill_container();
     // printing multimap multimap_high_score
     MultiMap::iterator itr;
     cout << "\tUser Name\tHIGH SCORE\n";
@@ -158,9 +155,6 @@ int read4file()
         cout << tmp_value << endl;
         tmp_value = numeric_limits<int>::max();
     }
-
-
-    cout << endl;
 
     return 0;
 }
