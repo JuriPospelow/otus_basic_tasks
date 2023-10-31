9a8e06dfb964466fd5f8c03d7e6a0f720d9b0ef4
Juri <jur@pospelow.de>
2023-10-31 08:05:55 +0100

    task3: introduce clean_file()

--- task3/03.homework_source-12995-dca15c/high_scores.cpp
+++ task3/03.homework_source-12995-dca15c/high_scores.cpp
@@ -24,6 +24,63 @@ void put_attempts_count(unsigned attempts_cnt)
     attempts_count = attempts_cnt;
 }
 
+int clean_file()
+{
+    typedef multimap<string, int> MultiMap;
+    MultiMap multimap_high_score;
+    std::fstream file{high_scores_filename};
+    if (!file.is_open()) {
+       std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
+       return -1;
+    }
+    // std::cout << "High scores table:" << std::endl;
+    std::string username;
+    int high_score = 0;
+    while (true) {
+        // Read the username first
+        file >> username;
+        // Read the high score next
+        file >> high_score;
+        // Ignore the end of line symbol
+        file.ignore();
+
+        if (file.fail()) {
+            break;
+        }
+        multimap_high_score.insert(pair<string, int>(username, high_score));
+    }
+    // printing multimap multimap_high_score
+    MultiMap::iterator itr;
+    // cout << "\tUser Name\tHIGH SCORE\n";
+
+    vector<string> v_moreThanOne;
+    string tmp{};
+
+    for (itr = multimap_high_score.begin(); itr != multimap_high_score.end(); ++itr) {
+        if(multimap_high_score.count(itr->first) == 1) {
+            file << itr->first << " " << itr->second
+                << '\n';
+        } else {
+            if (tmp != itr->first) v_moreThanOne.push_back(itr->first);
+            tmp = itr->first;
+      }
+    }
+    pair <MultiMap::iterator, MultiMap::iterator> ret;
+    int tmp_value{numeric_limits<int>::max()};
+    for (vector<string>::iterator itr = v_moreThanOne.begin(); itr != v_moreThanOne.end(); ++itr) {
+         file << *itr << " ";
+        ret = multimap_high_score.equal_range(*itr);
+        for (MultiMap::iterator it=ret.first; it!=ret.second; ++it) {
+            tmp_value = it->second < tmp_value ? it->second : tmp_value;
+        }
+        file << tmp_value << endl;
+        tmp_value = numeric_limits<int>::max();
+    }
+
+    return 0;
+    // cout << endl;
+}
+
     // Write new high score to the records table
 int write2file()
 {
@@ -38,6 +95,11 @@ int write2file()
     out_file << user_name << ' ';
     out_file << attempts_count;
     out_file << std::endl;
+
+    out_file.close();
+
+    clean_file();
+
     return 0;
 } // end of score here just to mark end of the logic block of code
 
