e9ecce04c603cecd579f18148d7f8d68eeab3505
Juri <jur@pospelow.de>
2023-11-09 17:15:30 +0100

    task5: init

---
+++ task5/05.homework/statistics.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <limits>
+
+class IStatistics {
+public:
+	virtual ~IStatistics() {}
+
+	virtual void update(double next) = 0;
+	virtual double eval() const = 0;
+	virtual const char * name() const = 0;
+};
+
+class Min : public IStatistics {
+public:
+	Min() : m_min{std::numeric_limits<double>::min()} {
+	}
+
+	void update(double next) override {
+		if (next < m_min) {
+			m_min = next;
+		}
+	}
+
+	double eval() const override {
+		return m_min;
+	}
+
+	const char * name() const override {
+		return "min";
+	}
+
+private:
+	double m_min;
+};
+
+int main() {
+
+	const size_t statistics_count = 1;
+	IStatistics *statistics[statistics_count];
+
+	statistics[0] = new Min{};
+
+	double val = 0;
+	while (std::cin >> val) {
+		for (size_t i = 0; i < statistics_count; ++i) {
+			statistics[i]->update(val);
+		}
+	}
+
+	// Handle invalid input data
+	if (!std::cin.eof() && !std::cin.good()) {
+		std::cerr << "Invalid input data\n";
+		return 1;
+	}
+
+	// Print results if any
+	for (size_t i = 0; i < statistics_count; ++i) {
+		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+	}
+
+	// Clear memory - delete all objects created by new
+	for (size_t i = 0; i < statistics_count; ++i) {
+		delete statistics[i];
+	}
+
+	return 0;
+}
