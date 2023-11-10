7474915a675954deda9ffa9822bb848ae9d78efe
Juri <jur@pospelow.de>
2023-11-10 16:58:27 +0100

    task5: statistics: introduce class Mean

--- task5/05.homework/statistics.cpp
+++ task5/05.homework/statistics.cpp
@@ -6,7 +6,7 @@ public:
 	virtual ~IStatistics() {}
 
 	virtual void update(double next) = 0;
-	virtual double eval() const = 0;
+	virtual double eval(double total = 0) const = 0;
 	virtual const char * name() const = 0;
 };
 
@@ -21,7 +21,7 @@ public:
 		}
 	}
 
-	double eval() const override {
+	double eval(double) const override {
 		return m_min;
 	}
 
@@ -44,7 +44,7 @@ public:
 		}
 	}
 
-	double eval() const override {
+	double eval(double) const override {
 		return m_max;
 	}
 
@@ -56,21 +56,44 @@ private:
 	double m_max;
 };
 
+class Mean : public IStatistics {
+public:
+	Mean() : m_mean{} {
+	}
+
+	void update(double next) override {
+		m_mean += next;
+	}
+
+	double eval(double total = 1) const override {
+		return m_mean/total;
+	}
+
+	const char * name() const override {
+		return "mean";
+	}
+
+private:
+	double m_mean{};
+};
+
 int main() {
 
-	const size_t statistics_count = 2;
+	const size_t statistics_count = 3;
 	IStatistics *statistics[statistics_count];
 
 	statistics[0] = new Min{};
 	statistics[1] = new Max{};
+	statistics[2] = new Mean{};
 
 	double val = 0;
+	double total = 0;
 	while (std::cin >> val) {
 		for (size_t i = 0; i < statistics_count; ++i) {
 			statistics[i]->update(val);
 		}
+        ++total;
 	}
-
 	// Handle invalid input data
 	if (!std::cin.eof() && !std::cin.good()) {
 		std::cerr << "Invalid input data\n";
@@ -79,7 +102,7 @@ int main() {
 
 	// Print results if any
 	for (size_t i = 0; i < statistics_count; ++i) {
-		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+        std::cout << statistics[i]->name() << " = " << statistics[i]->eval(total) << std::endl;
 	}
 
 	// Clear memory - delete all objects created by new
