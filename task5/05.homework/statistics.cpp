0bf5b4db203e8c380037220409b499570b177800
Juri <jur@pospelow.de>
2023-11-10 16:36:33 +0100

    task5: statistics: introduce class Max and change EOL to Unix

--- task5/05.homework/statistics.cpp
+++ task5/05.homework/statistics.cpp
@@ -12,7 +12,7 @@ public:
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
+	Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -33,12 +33,36 @@ private:
 	double m_min;
 };
 
+class Max : public IStatistics {
+public:
+	Max() : m_max{std::numeric_limits<double>::min()} {
+	}
+
+	void update(double next) override {
+		if (next > m_max) {
+			m_max = next;
+		}
+	}
+
+	double eval() const override {
+		return m_max;
+	}
+
+	const char * name() const override {
+		return "max";
+	}
+
+private:
+	double m_max;
+};
+
 int main() {
 
-	const size_t statistics_count = 1;
+	const size_t statistics_count = 2;
 	IStatistics *statistics[statistics_count];
 
 	statistics[0] = new Min{};
+	statistics[1] = new Max{};
 
 	double val = 0;
 	while (std::cin >> val) {
